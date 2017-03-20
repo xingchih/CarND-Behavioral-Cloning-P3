84e7ee6cff911a33a79d1a1d7327a28b12e573c7
xingchih <he.xingchi@gmail.com>
2017-03-20 00:19:37 +0000

    updated model.py and h5

--- model.py
+++ model.py
@@ -101,7 +101,7 @@ model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch = len(train_samples),    \
                                      validation_data   = validation_generator,  \
                                      nb_val_samples    = len(validation_samples), 
-                                     nb_epoch          = 3)
+                                     nb_epoch          = 10)
 # save model
 model.save('model.h5')
 
